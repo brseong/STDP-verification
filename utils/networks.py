c2c646f5a58e1794481419c7da10d49276d05192
Baek Ryun Seong <sizzflair97@gmail.com>
2024-10-01 20:40:11 +0900

    241001

--- utils/networks.py
+++ utils/networks.py
@@ -1,5 +1,7 @@
 import torch
+
 from torch import nn
+from torch.nn import Parameter
 from typeguard import typechecked
 from typing import Callable, cast
 from abc import abstractmethod, ABCMeta
@@ -8,6 +10,8 @@ from utils.spikingjelly.spikingjelly.activation_based.surrogate import Sigmoid
 from .types import Tensor2D
 from .dataclasses import DistInfo
 from .spikingjelly.spikingjelly.activation_based import layer, learning, neuron
+from .SpykeTorch.SpykeTorch import snn
+from .SpykeTorch.SpykeTorch import functional as sf
 
 def stdp_DiehlAndCook2015():
     pass
@@ -48,9 +52,138 @@ class STDPNet(nn.Module, metaclass=ABCMeta):
     @abstractmethod
     def f_post(x, w_max, alpha=0.) -> float: pass
     
-class Kheradpisheh(STDPNet):
+class Mozafari2018(STDPNet):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        
+        self.conv1 = snn.Convolution(6, 30, 5, 0.8, 0.05)
+        self.conv1_t = 15
+        self.k1 = 5
+        self.r1 = 3
+
+        self.conv2 = snn.Convolution(30, 250, 3, 0.8, 0.05)
+        self.conv2_t = 10
+        self.k2 = 8
+        self.r2 = 1
+
+        self.conv3 = snn.Convolution(250, 200, 5, 0.8, 0.05)
+
+        self.stdp1 = snn.STDP(self.conv1, (0.004, -0.003))
+        self.stdp2 = snn.STDP(self.conv2, (0.004, -0.003))
+        self.stdp3 = snn.STDP(self.conv3, (0.004, -0.003), False, 0.2, 0.8)
+        self.anti_stdp3 = snn.STDP(self.conv3, (-0.004, 0.0005), False, 0.2, 0.8)
+        self.max_ap = Parameter(torch.Tensor([0.15]))
+
+        self.decision_map = []
+        for i in range(10):
+            self.decision_map.extend([i]*20)
+
+        self.ctx = {"input_spikes":None, "potentials":None, "output_spikes":None, "winners":None}
+        self.spk_cnt1 = 0
+        self.spk_cnt2 = 0
+        
+    def forward(self, input, max_layer):
+        input = sf.pad(input.float(), (2,2,2,2), 0)
+        if self.training:
+            pot = self.conv1(input)
+            spk, pot = sf.fire(pot, self.conv1_t, True)
+            if max_layer == 1:
+                self.spk_cnt1 += 1
+                if self.spk_cnt1 >= 500:
+                    self.spk_cnt1 = 0
+                    ap = torch.tensor(self.stdp1.learning_rate[0][0].item(), device=self.stdp1.learning_rate[0][0].device) * 2
+                    ap = torch.min(ap, self.max_ap)
+                    an = ap * -0.75
+                    self.stdp1.update_all_learning_rate(ap.item(), an.item())
+                pot = sf.pointwise_inhibition(pot)
+                spk = pot.sign()
+                winners = sf.get_k_winners(pot, self.k1, self.r1, spk)
+                self.ctx["input_spikes"] = input
+                self.ctx["potentials"] = pot
+                self.ctx["output_spikes"] = spk
+                self.ctx["winners"] = winners
+                return spk, pot
+            spk_in = sf.pad(sf.pooling(spk, 2, 2), (1,1,1,1))
+            pot = self.conv2(spk_in)
+            spk, pot = sf.fire(pot, self.conv2_t, True)
+            if max_layer == 2:
+                self.spk_cnt2 += 1
+                if self.spk_cnt2 >= 500:
+                    self.spk_cnt2 = 0
+                    ap = torch.tensor(self.stdp2.learning_rate[0][0].item(), device=self.stdp2.learning_rate[0][0].device) * 2
+                    ap = torch.min(ap, self.max_ap)
+                    an = ap * -0.75
+                    self.stdp2.update_all_learning_rate(ap.item(), an.item())
+                pot = sf.pointwise_inhibition(pot)
+                spk = pot.sign()
+                winners = sf.get_k_winners(pot, self.k2, self.r2, spk)
+                self.ctx["input_spikes"] = spk_in
+                self.ctx["potentials"] = pot
+                self.ctx["output_spikes"] = spk
+                self.ctx["winners"] = winners
+                return spk, pot
+            spk_in = sf.pad(sf.pooling(spk, 3, 3), (2,2,2,2))
+            pot = self.conv3(spk_in)
+            spk = sf.fire(pot)
+            winners = sf.get_k_winners(pot, 1, 0, spk)
+            self.ctx["input_spikes"] = spk_in
+            self.ctx["potentials"] = pot
+            self.ctx["output_spikes"] = spk
+            self.ctx["winners"] = winners
+            output = -1
+            if len(winners) != 0:
+                output = self.decision_map[winners[0][0]]
+            return output
+        else:
+            pot = self.conv1(input)
+            spk, pot = sf.fire(pot, self.conv1_t, True)
+            if max_layer == 1:
+                return spk, pot
+            pot = self.conv2(sf.pad(sf.pooling(spk, 2, 2), (1,1,1,1)))
+            spk, pot = sf.fire(pot, self.conv2_t, True)
+            if max_layer == 2:
+                return spk, pot
+            pot = self.conv3(sf.pad(sf.pooling(spk, 3, 3), (2,2,2,2)))
+            spk = sf.fire(pot)
+            winners = sf.get_k_winners(pot, 1, 0, spk)
+            output = -1
+            if len(winners) != 0:
+                output = self.decision_map[winners[0][0]]
+            return output
+    
+    def stdp(self, layer_idx):
+        if layer_idx == 1:
+            self.stdp1(self.ctx["input_spikes"], self.ctx["potentials"], self.ctx["output_spikes"], self.ctx["winners"])
+        if layer_idx == 2:
+            self.stdp2(self.ctx["input_spikes"], self.ctx["potentials"], self.ctx["output_spikes"], self.ctx["winners"])
+
+    def update_learning_rates(self, stdp_ap, stdp_an, anti_stdp_ap, anti_stdp_an):
+        self.stdp3.update_all_learning_rate(stdp_ap, stdp_an)
+        self.anti_stdp3.update_all_learning_rate(anti_stdp_an, anti_stdp_ap)
+
+    def reward(self):
+        self.stdp3(self.ctx["input_spikes"], self.ctx["potentials"], self.ctx["output_spikes"], self.ctx["winners"])
+
+    def punish(self):
+        self.anti_stdp3(self.ctx["input_spikes"], self.ctx["potentials"], self.ctx["output_spikes"], self.ctx["winners"])
+    
+    def post_optim(self) -> None:
+        pass
+    
+    def draw_weights(self, id:int=0) -> Tensor2D:
+        pass
+    
+    @staticmethod
+    def f_weight(x) -> torch.Tensor:
+        raise NotImplementedError
+    
+    @staticmethod
+    def f_pre(x, w_min, alpha=0.) -> float:
+        raise NotImplementedError
+
+    @staticmethod
+    def f_post(x, w_max, alpha=0.) -> float:
+        raise NotImplementedError
 
 class DiehlAndCook2015(STDPNet):
     draw_ids = (0,1)
